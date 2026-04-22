9c10969acf71c951e12ff19e745e8ca9b6aa6ff8
guodk2003 <gdkhnu2003@163.com>
2026-04-23 00:05:49 +0800

    Site updated: 2026-04-23 00:05:48

---
+++ js/links.js
@@ -0,0 +1,39 @@
+// // 随机排列
+// function shuffle(arr) {
+//     let i = arr.length;
+//     while (i) {
+//       let j = Math.floor(Math.random() * i--);
+//       [arr[j], arr[i]] = [arr[i], arr[j]];
+//     }
+//   }
+  
+  // 渲染数据
+  // function renderlink(data) {
+  //     var name, avatar, site, li = "";
+  //     shuffle(data);
+  //     for (var i = 0; i < data.length; i++) {
+  //         name = data[i].name;
+  //         avatar = data[i].avatar;
+  //         site = data[i].site;
+  //         li += '<div class="card">' + '<a href="' + site + '" target="_blank">' + '<div class="thumb" style="background: url( ' + avatar + ');">' + '</div>' + '</a>' + '<div class="card-header">' + '<div><a href="' + site + '" target="_blank">' + name + '</a></div>' + '</div>' + '</div>';
+  //     }
+  //     document.querySelector(".link-navigation").innerHTML = li;
+  // }
+  function renderlink(data) {
+    shuffle(data);
+    const linkContainer = document.querySelector(".link-navigation");
+    linkContainer.innerHTML = data.map(item => `
+      <div class="card">
+        <div class="thumb" style="background-image: url('${item.avatar}');"></div>
+        <div class="card-header">
+          <a href="${item.site}" target="_blank">${item.name}</a>
+        </div>
+      </div>
+    `).join('');
+  }
+  
+  // 获取 json 文件
+  fetch('/links/linklist.json')
+    .then(response => response.json())
+    .then(res => renderlink(res));
+  
